7fd25bc65e6c515b89f0982646dc23adea2959bd
sivagami.rm <sivagami.rm@kgisl.com>
2022-07-29 15:06:44 +0530

    change v2

--- multitenancy-demo/src/main/java/com/n47/multitenancydemo/MultitenancyDemoApplication.java
+++ multitenancy-demo/src/main/java/com/n47/multitenancydemo/MultitenancyDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MultitenancyDemoApplication {
-//testpop
+//testpo
     public static void main(String[] args) {
         SpringApplication.run(MultitenancyDemoApplication.class, args);
     }
