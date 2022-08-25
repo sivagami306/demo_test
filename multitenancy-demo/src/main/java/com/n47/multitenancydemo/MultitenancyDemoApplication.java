f01014ab66307e9b765a539d1cac7d0816ba150d
sivagami.rm <sivagami.rm@kgisl.com>
2022-08-25 08:09:11 +0530

    Testing v5

--- multitenancy-demo/src/main/java/com/n47/multitenancydemo/MultitenancyDemoApplication.java
+++ multitenancy-demo/src/main/java/com/n47/multitenancydemo/MultitenancyDemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MultitenancyDemoApplication {
+	
     public static void main(String[] args) {
         SpringApplication.run(MultitenancyDemoApplication.class, args);
     }
